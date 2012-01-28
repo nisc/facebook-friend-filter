dbb5de28d9aed0d8388aeefd5137bd35a06ca905
Niklas Schmücker <nschmuecker@gmail.com>
2012-01-28 01:02:32 +0100

    Hack in jQ-UI based autocomplete for country names

--- static/index.js
+++ static/index.js
@@ -80,3 +80,64 @@ window.fbAsyncInit = function() {
   js.src = "//connect.facebook.net/en_US/all.js";
   d.getElementsByTagName('head')[0].appendChild(js);
 }(document));
+
+// Autocomplete experiment for countries. Gets data via Graph search.
+$(function() {
+  function split( val ) {
+    return val.split( /,\s*/ );
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+
+  $( 'input[name=countries]' )
+  // don't navigate away from the field on tab when selecting an item
+  .bind( 'keydown', function( event ) {
+    if ( event.keyCode === $.ui.keyCode.TAB &&
+        $( this ).data( 'autocomplete' ).menu.active ) {
+      event.preventDefault();
+    }
+  })
+  .autocomplete({
+    source: function( request, response ) {
+      $.getJSON( 'https://graph.facebook.com/search', {
+        q: extractLast( request.term ),
+        type: 'adcountry'
+      },
+      function(data) {
+        var d = data.data
+        , resultset = [];
+        for (var i=0; i < d.length; i++) {
+          resultset.push({
+            id: d[i].country_code,
+            label: d[i].name,
+            value: d[i].name
+          });
+        }
+        response(resultset);
+      });
+    },
+    search: function() {
+      // custom minLength
+      var term = extractLast( this.value );
+      if ( term.length < 2 ) {
+        return false;
+      }
+    },
+    focus: function() {
+      // prevent value inserted on focus
+      return false;
+    },
+    select: function( event, ui ) {
+      var terms = split( this.value );
+      // remove the current input
+      terms.pop();
+      // add the selected item
+      terms.push( ui.item.value );
+      // add placeholder to get the comma-and-space at the end
+      terms.push( '' );
+      this.value = terms.join( ', ' );
+      return false;
+    }
+  });
+});

3100c4a3383a4443ec9c298b1a1cc8a8cbbcaa14
Niklas Schmücker <nschmuecker@gmail.com>
2012-01-29 03:38:27 +0100

    Add dialog windows

--- static/index.js
+++ static/index.js
@@ -7,18 +7,40 @@ window.fbAsyncInit = function() {
     oauth      : true,
   });
 
-  $('#create_list').submit(function(e) {
+  var status_msg  = $('#status_msg')
+    , form        = $('#create_list')
+    , countries   = form.find('input[name="countries"]')
+    , languages   = form.find('input[name="languages"]')
+    , single      = form.find('input[name="single"]')
+    , age_min     = form.find('input[name="age_min"]')
+    , age_max     = form.find('input[name="age_max"]');
+
+  var dialog1 = $('#dialog_list').dialog({
+    autoOpen: false,
+    resizable: false,
+    buttons: {
+      'Create it!': function() { form.submit(); $(this).dialog('close'); }
+    }
+  });
+
+  var dialog2 = $('#dialog_post').dialog({
+    autoOpen: false,
+    resizable: false,
+    buttons: {
+      'Post it!': function() { form.submit(); $(this).dialog('close'); }
+    }
+  });
+
+  var submit1 = dialog1.find('input[name="submit"]')
+    , name1   = dialog1.find('input[name="list_name"]')
+    , submit2 = dialog2.find('input[name="submit"]')
+    , message = dialog2.find('textarea');
+
+  $('input[name=make_friendlist]').click(function() { dialog1.dialog('open') });
+  $('input[name=make_wallpost]').click(function() { dialog2.dialog('open') });
+
+  form.submit(function(e) {
     e.preventDefault();
-
-    var status_msg = $('#status_msg');
-    var form = $(this);
-    var name = form.find('input[name="list_name"]');
-    var countries = form.find('input[name="countries"]');
-    var languages = form.find('input[name="languages"]');
-    var single = form.find('input[name="single"]');
-    var age_min = form.find('input[name="age_min"]');
-    var age_max = form.find('input[name="age_max"]');
-
     var perms = ['manage_friendlists', 'read_friendlists'];
     if(languages.val()) perms.push('friends_likes');
     if(countries.val()) perms = perms.concat(['friends_location',
@@ -26,50 +48,50 @@ window.fbAsyncInit = function() {
       if(single.val()) perms.push('friends_relationships');
       if(age_min.val() || age_max.val()) perms.push('friends_birthday');
 
-      if(name.val()) {
-        form.slideUp('slow');
-        status_msg.fadeOut('slow', function() {
-          status_msg.text('Getting authorization ..').fadeIn('slow');
-        });
-
-        FB.login(function(response) {
-          if (response.authResponse) {
-            status_msg.fadeOut('slow', function() {
-              status_msg.text('Loading ..').fadeIn('slow');
-            });
-
-            $.post('./create', form.serialize()).success(function(data) {
-              var list_id = data['list_id'];
-              status_msg.fadeOut('slow', function() {
-                status_msg
-                .html('Success! Go to <a href="https://facebook.com/lists/'
-                +list_id+'">Facebook</a> and enjoy.')
-                .fadeIn('slow');
-              });
-              console.log('Holy shit, it worked!');
-            }).fail(function() {
-              status_msg.fadeOut('slow', function() {
-                status_msg
-                .text('Failure! Try again?')
-                .fadeIn('slow', function() {
-                  form.slideDown('slow');
-                });
-              });
-              console.log('Unholy shit, it did not work!');
-            }).always(function() {
-              // pass
-            });
-          } else { // user did not authorize
-            status_msg.fadeOut('slow', function() {
-              status_msg.text('No permissions, no fun. Sorry.').fadeIn('slow');
-            });
-            form.slideDown('slow');
-          }
-        }, {scope: perms.join(',')}); // only request required permissions
-      } else {
-        name.val('Can I please have a name?');
+      if(!name1.val()) {
+        name1.val(String(new Date().getTime()));
       }
-      return false;
+
+      form.slideUp('slow');
+      status_msg.fadeOut('slow', function() {
+        status_msg.text('Getting authorization ..').fadeIn('slow');
+      });
+
+      FB.login(function(response) {
+        if (response.authResponse) {
+          status_msg.fadeOut('slow', function() {
+            status_msg.text('Loading ..').fadeIn('slow');
+          });
+
+          $.post('./create', form.serialize()).success(function(data) {
+            var list_id = data['list_id'];
+            status_msg.fadeOut('slow', function() {
+              status_msg
+              .html('Success! Go to <a href="https://facebook.com/lists/'
+              +list_id+'">Facebook</a> and enjoy.')
+              .fadeIn('slow');
+            });
+            console.log('Holy shit, it worked!');
+          }).fail(function() {
+            status_msg.fadeOut('slow', function() {
+              status_msg
+              .text('Failure! Try again?')
+              .fadeIn('slow', function() {
+                form.slideDown('slow');
+              });
+            });
+            console.log('Unholy shit, it did not work!');
+          }).always(function() {
+            // pass
+          });
+        } else { // user did not authorize
+          status_msg.fadeOut('slow', function() {
+            status_msg.text('No permissions, no fun. Sorry.').fadeIn('slow');
+          });
+          form.slideDown('slow');
+        }
+      }, {scope: perms.join(',')}); // only request required permissions
+    return false;
   });
 };
 
